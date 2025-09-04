b8207e395a926833f137156a8268ab95db04e160
Bá Oai <baoai25.work@gmail.com>
2025-09-04 17:45:10 +0700

    init atherlabs_test module

---
+++ Source/atherlabs_test/atherlabs_test.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "atherlabs_test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, atherlabs_test, "atherlabs_test" );
